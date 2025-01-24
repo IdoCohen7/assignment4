02abe33613c038938a3048cd5cf0e6cedffd36f7
IdoCohen7 <idocohen727@gmail.com>
2025-01-24 19:12:40 +0200

    Initial Commit

---
+++ recipe-book/app/(tabs)/plane.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Plane(props) {
+  return (
+    <View>
+      <Text>plane</Text>
+    </View>
+  );
+}
---
+++ recipe-book/app/(tabs)/settings.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Settings(props) {
+  return (
+    <View>
+      <Text>settings</Text>
+    </View>
+  );
+}
